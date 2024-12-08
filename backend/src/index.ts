b09482cc9206653058cc2621ce049004970aa545
dhanushbd03 <dhanushrocks03@gmail..com>
2024-12-09 00:59:34 +0530

    added cors to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,15 +10,15 @@ const app: Application = express();
 const port: number = Number(process.env.PORT) || 3000;
 app.use(
   cors({
-    origin: "https://twnewsdhanushbd.vercel.app",
+    origin: ["http://localhost:5173", "https://twnewsdhanushbd.vercel.app/"],
   })
 );
-app.use(errorHandler);
 app.use(express.json());
 db();
 
 app.use("/api", newsRouter);
 app.use("/api", userRouter);
+app.use(errorHandler); // Add error handler middleware
 app.listen(port, () => {
   console.log(`Server is running on port ${port} : http://localhost:${port}`);
 });
