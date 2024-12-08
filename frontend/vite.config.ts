6ce1b3b9400af2160ee3041c285de81f9597c395
dhanushbd03 <dhanushrocks03@gmail..com>
2024-12-08 20:38:56 +0530

    correct build errors 2

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://twnews.onrender.com",
         changeOrigin: true,
       },
     },
