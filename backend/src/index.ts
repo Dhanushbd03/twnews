74fa05e7d4244c5b56852517cee4a44a978e83e2
dhanushbd03 <dhanushrocks03@gmail..com>
2024-12-09 02:30:59 +0530

    added cors to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ const app: Application = express();
 const port: number = Number(process.env.PORT) || 3000;
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://twnewsdhanushbd.vercel.app/"],
+    origin: ["http://localhost:5173", "https://twnewsdhanushbd.vercel.app"],
   })
 );
 app.use(express.json());
